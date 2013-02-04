6ba2ca1581fa614031cacec41d435dfcbef469cb
= <=>
2013-02-04 11:54:51 -0700

    Completed decoupling from IoC. Added wireup dependency.

--- FlitBit.Dto/FlitBit.Dto.Tests/DtoFactoryTests.cs
+++ FlitBit.Dto/FlitBit.Dto.Tests/DtoFactoryTests.cs
@@ -1,4 +1,6 @@
-﻿using FlitBit.Dto.Tests.Model;
+﻿using FlitBit.Core;
+using FlitBit.Dto.Tests.Model;
+using FlitBit.Wireup;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FlitBit.Dto.Tests
@@ -9,7 +11,12 @@ namespace FlitBit.Dto.Tests
 	[TestClass]
 	public class DtoFactoryTests
 	{
-		
+		[TestInitialize]
+		public void Init()
+		{
+			WireupCoordinator.SelfConfigure();
+		}
+
 		/// <summary>
 		///Gets or sets the test context which provides
 		///information about and functionality for the current test run.
@@ -18,8 +25,9 @@ namespace FlitBit.Dto.Tests
 
 		[TestMethod]
 		public void DtoFactory_CanCreateInstances()
-		{			
-			var it = DtoFactory.CreateInstance<IJustAnID>();
+		{
+			var factory = FactoryFactory.Instance;
+			var it = factory.CreateInstance<IJustAnID>();
 			Assert.IsNotNull(it);
 		}
 	}
---
+++ FlitBit.Dto/Wireup.cs
@@ -0,0 +1,25 @@
+﻿using FlitBit.Core;
+using FlitBit.Wireup;
+using FlitBit.Wireup.Meta;
+using System;
+
+[assembly: WireupDependency(typeof(FlitBit.Emit.WireupThisAssembly))]
+[assembly: Wireup(typeof(FlitBit.Dto.WireupThisAssembly))]
+
+namespace FlitBit.Dto
+{
+	/// <summary>
+	/// Wires up this assembly.
+	/// </summary>
+	[CLSCompliant(false)]
+	public sealed class WireupThisAssembly : IWireupCommand
+	{
+		/// <summary>
+		/// Wires up this assembly.
+		/// </summary>
+		/// <param name="coordinator"></param>
+		public void Execute(IWireupCoordinator coordinator)
+		{
+		}
+	}
+}
