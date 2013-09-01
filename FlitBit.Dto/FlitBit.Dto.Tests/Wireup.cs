0e9d81143d0e77453b4d4e93a0152ae6a6221bae
Phillip Clark <phillip@flitbit.com>
2013-09-01 10:36:21 -0600

    Updated references to latest. Fixed wireup.

--- FlitBit.Dto/FlitBit.Dto.Tests/Wireup.cs
+++ FlitBit.Dto/FlitBit.Dto.Tests/Wireup.cs
@@ -2,3 +2,5 @@
 
 // Any assembly dependent on DTO will need this declaration:
 [assembly: WireupDependency(typeof(FlitBit.Dto.AssemblyWireup))]
+// This declaration signifies this assembly is dependent on IoC being wired.
+[assembly: WireupDependency(typeof(FlitBit.IoC.AssemblyWireup))]
